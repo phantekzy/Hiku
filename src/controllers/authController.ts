46c70101ff2a842e617ce1c7ad289ae5b7d76ce3
phantekzy <mainilotfi@gmail.com>
2026-04-21 20:42:50 +0100

    Authentication Controller : - Login schema is done

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -12,3 +12,8 @@ const registerSchema = z.object({
     ),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
+
+const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(1, "Password is required"),
+});
