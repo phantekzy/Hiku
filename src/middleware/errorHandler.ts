4efc2776ff0f8977176ea75ee4ad524dd6dcf74f
phantekzy <mainilotfi@gmail.com>
2026-04-21 20:23:38 +0100

    Global Error Handler -Error handler method

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,3 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+
 export interface AppError extends Error {
   statusCode?: number;
 }
+
+export const errorHandler = (
+  err: AppError,
+  _req: Request,
+  _res: Response,
+  _next: NextFunction,
+): void => {};
