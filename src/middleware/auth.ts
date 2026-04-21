660399a1d61ed0586d1e4c42d3df51b4d8bc162f
phantekzy <mainilotfi@gmail.com>
2026-04-21 18:31:45 +0100

    authenticate function

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
 interface JwtPayload {
   id: string;
   email: string;
   username: string;
 }
+
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {};
