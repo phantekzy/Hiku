f3b65af903f1286797b512cf36d561e0139a5c39
phantekzy <mainilotfi@gmail.com>
2026-04-24 20:51:24 +0100

    Canvas Resource Router is done lhmoudoulilah :   - Yes same thing this is my way i imagine the tree then i build step by step

--- src/routes/canvases.ts
+++ src/routes/canvases.ts
@@ -1,3 +1,13 @@
 import { Router } from "express";
 
 const router = Router();
+
+router.use(authenticate);
+
+router.get("/", getCanvases);
+router.get("/:id", getCanvas);
+router.post("/", createCanvas);
+router.patch("/:id", updateCanvas);
+router.delete("/:id", deleteCanvas);
+
+export default router;
