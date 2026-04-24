e1cfa498ec3b3ab397c5b75a6deae1deb891028c
phantekzy <mainilotfi@gmail.com>
2026-04-24 21:01:31 +0100

    Diagram controler is done elhamdoulilah :   - I will make the files and dirs when i want

--- src/routes/diagrams.ts
+++ src/routes/diagrams.ts
@@ -1,3 +1,13 @@
 import { Router } from "express";
 
 const router = Router();
+router.use(authenticate);
+
+// will add this later
+router.get("/", getDiagrams);
+router.get("/:id", getDiagram);
+router.post("/", createDiagram);
+router.patch("/:id", updateDiagram);
+router.delete("/:id", deleteDiagram);
+
+export default router;
