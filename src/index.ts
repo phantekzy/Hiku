264ddd83301350f75556df7e308f91ae4c57b26b
phantekzy <mainilotfi@gmail.com>
2026-04-24 21:10:43 +0100

    Hiku entry point :   - added a port   - added the middleware

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
