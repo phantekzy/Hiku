c7258a83a8a858c2f2c295ce45dd3fedaf952efe
phantekzy <mainilotfi@gmail.com>
2026-04-24 20:47:08 +0100

    Document Resource Router is done elhndoulilah :   - Same thing i will add the files and directories later

--- src/routes/documents.ts
+++ src/routes/documents.ts
@@ -1,3 +1,12 @@
 import { Router } from "express";
 
 const router = Router();
+router.use(authenticate);
+
+router.get("/", getDocuments);
+router.get("/:id", getDocument);
+router.post("/", createDocument);
+router.patch("/:id", updateDocument);
+router.delete("/:id", deleteDocument);
+
+export default router;
