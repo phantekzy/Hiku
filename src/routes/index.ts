bcf9f9c08ea8023069e29a2a42b53d54306e6cc0
phantekzy <mainilotfi@gmail.com>
2026-04-24 21:07:11 +0100

    Main api router :   - i hate when i have to repeat that i will add those later when time comes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,11 @@
 import { Router } from "express";
 
 const router = Router();
+
+// Will add this mfs later
+router.use("/auth", authRoutes);
+router.use("/documents", documentRoutes);
+router.use("/canvases", canvasRoutes);
+router.use("/diagrams", diagramRoutes);
+
+export default router;
