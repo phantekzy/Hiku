11068a5cc1a325ddf21cc889f754301408b46b04
phantekzy <mainilotfi@gmail.com>
2026-04-28 20:18:10 +0100

    date formating

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
+
+export function formatDate(date: string | Date): string {
+  return new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  }).format(new Date(date));
+}
