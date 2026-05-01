114c03bf92aaab406c2e2d353e7752281ceb2971
phantekzy <mainilotfi@gmail.com>
2026-05-01 16:32:08 +0100

    useAuth

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,1 +1,7 @@
-export function useAuth() {}
+import { useStore } from "zustand";
+import { useNavigate } from "react-router-dom";
+
+export function useAuth() {
+  const { user, token, setAuth, clearAuth, isAuthenticated } = useStore();
+  const navigate = useNavigate();
+}
