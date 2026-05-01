577d69a96296336e35685e4a8ce31181fb9992bc
phantekzy <mainilotfi@gmail.com>
2026-05-01 16:11:07 +0100

    API    - request method

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -3,3 +3,8 @@ const BASE_URL = "/api";
 function getToken(): string | null {
   return localStorage.getItem("hiku_token");
 }
+
+async function request<T>(
+  endpoint: string,
+  options: RequestInit = {},
+): Promise<T> {}
